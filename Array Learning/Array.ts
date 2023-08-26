e410f5827e41a050a963170ff317e76a0c070bb8
1122developers <143153964+1122developers@users.noreply.github.com>
2023-08-25 22:52:04 -0700

    Add files via upload

---
+++ Array Learning/Array.ts
@@ -0,0 +1,20 @@
+// var userName:string=("Zeeshan Shafi")
+// console.log(userName);
+var array: number[] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+console.log(array.length),
+    // splice is use for to delete value in index//
+    array.splice(5, 0, 51, 52, 53, 54, 55, 56, 57, 58, 59);
+console.log(array)
+// under the line splice method is use to delete //
+array.splice(5, 2);
+console.log(array);
+console.log(array.length);
+//in array push comand is use to add index in array last//
+array.push(101, 102)
+console.log(array);
+array.pop()
+console.log(array);
+array.unshift(1, 2, 3, 4, 5, 6, 7, 8, 9),
+    console.log(array)
+array.shift()
+console.log(array)
